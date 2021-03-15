4a11fd32c63dc8093350b55256bec28ac04bb0d1
stecyka <andrzej.stecyk@play.pl>
2021-03-15 11:52:46 +0100

    added support for bash and sh scripts

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
 #!/usr/bin/python3.4
+import shutil
+from pathlib import Path
 
 
-def add_marker():
-    print(dupa)
-    "#!/bin/bash"
-    "#!/bin/sh"
-    pass
+def add_marker(file):
+    sh = ['#!/bin/bash', '#!/bin/sh']
+    sh_str = "echo $(date '+%Y%m%d%H%M%S') module=$(readlink -f $0) pid=$$ host=$(uname -n)"
+    with open(str(file.absolute()), 'r') as fp:
+        content = fp.readlines()
+
+    if len(content) > 0:
+        print(content[0])
+        if content[0].strip() in sh:
+            content.insert(1, sh_str + '\n')
+            with open(str(file.absolute()), 'w') as frp:
+                content = "".join(content)
+                frp.write(content)
 
 
-def make_backup():
-    pass
+def make_backup(file):
+    dest = Path(str(file.absolute()) + '_python_bac')
+    if dest.exists():
+        raise FileExistsError("make_backup", "Can't copy a file, backup already exists")
+
+    shutil.copy(file, dest)
+
+    if not dest.exists():
+        raise FileNotFoundError("make_backup", "Could not create a backup")
 
 
-def find_files():
-    pass
+if __name__ == '__main__':
+    base = Path("Z:/tmp")
+    extensions = ['.sh']
+    for somefile in base.glob('**/*'):
+        if somefile.suffix in extensions:
+            try:
+                make_backup(somefile)
+            except FileExistsError:
+                continue
+                # TODO
+
+            except FileNotFoundError:
+                continue
+                # TODO
+
+            add_marker(somefile)
---
+++ sandbox.py
@@ -0,0 +1,6 @@
+a = "#!/bin/sh"
+
+b = ["asdasdasd", "asdsadasd", "#!/bin/sh", "sadsdasd"]
+
+if a in b:
+    print('asdasd')
